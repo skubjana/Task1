795eb952f4d66f978efdf110734d05a2c5e2d5a9
skubjana <skubjana@varsitycollege.co.za>
2023-03-27 09:27:01 +0200

    Real Commit

---
+++ src/customdislpay/CustomDislpay.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package customdislpay;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author lab_services_student
+ */
+public class CustomDislpay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hello, today is Thursday");
+    }
+    
+}
